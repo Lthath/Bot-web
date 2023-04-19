f940aef10d9747dd403a4bcbe0194813fdb10696
Lthath <hakim.afarmach@supdevinci-edu.fr>
2023-04-19 09:41:32 +0200

    Finalisation du Script

--- main.cjs
+++ main.cjs
@@ -4,7 +4,6 @@ const now = new Date();
 const hours = now.getHours();
 const minutes = now.getMinutes();
 const seconds = now.getSeconds();
-console.log(`Il est ${hours}:${minutes}:${seconds}`);
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -28,18 +27,27 @@ console.log(`Il est ${hours}:${minutes}:${seconds}`);
       : hours >= 13 && hours <= 17
       ? `/html/body/div[3]/div/div/div[5]/div/div[1]/div[2]/table/tbody/tr[2]/td[3]/a`
       : null;
-  await page.click("#seances_view_passed"); // Test
-  await page.waitForNavigation({ waitUntil: "networkidle0" }); // Attente de la nouvelle page
   if (buttonSelector) {
     const [button] = await page.$x(buttonSelector); // Utilisation de destructuring pour extraire le premier élément du tableau
     if (button) {
       await button.click();
-      console.log(`Clic sur ${buttonSelector}`);
     } else {
       console.error("Le bouton n'a pas été trouvé");
     }
-    //await page.reload({ waitUntil: "networkidle0" });
-    console.log(button);
-    /*await browser.close();*/
+  } else {
+    console.error("Hors cours");
+    await browser.close();
   }
+  while (true) {
+    await page.reload({ waitUntil: "networkidle0" });
+    try {
+      await page.waitForSelector(".btn-success", { timeout: 1000 });
+      break; // Sortir de la boucle si le bouton est présent
+    } catch (error) {
+      console.log(
+        "Le bouton n'est pas encore présent, rechargement de la page..."
+      );
+    }
+  }
+  await page.click(".btn-success");
 })();
